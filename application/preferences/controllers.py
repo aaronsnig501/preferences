4da77f15db3eb2dfcc2b9990c7dfe260ed8bd05d
Aaron Sinnott <aaronsinnott93@gmail.com>
2024-11-17 09:54:30 +0000

    [#12] - Tidy up code and add docstrings

--- application/preferences/controllers.py
+++ application/preferences/controllers.py
@@ -9,7 +9,6 @@ from .validators import PreferencesRequestBody
 bp = Blueprint("preferences", url_prefix="/api/preferences")
 
 
-
 @bp.post("/create")
 @validate(json=PreferencesRequestBody)
 async def create_preferences(
@@ -17,5 +16,29 @@ async def create_preferences(
     body: PreferencesRequestBody,
     preferences_manager: PreferencesManager
 ) -> SanicResponse:
+    """Create preferences
+
+    Store a user's preferences in the database
+
+    Example Usage:
+        ```sh
+            curl --header "Content-Type: application/json" \
+                --request POST \
+                --data \
+                    '{ \
+                        "to_language": "en", \
+                        "from_language": "en", \
+                        "part_of_speech_tagger": "aws", \
+                        "client_id": "d807e048-d118-4331-a8e6-d59cbe62cbd4"\
+                    }' http://localhost:8002/api/preferences/create | jq
+        ```
+
+    Example Response:
+        ```json
+        {
+            "message": "SUCCESS"
+        }
+        ```
+    """
     await preferences_manager.set_preferences(body)
     return json({"message": "SUCCESS"}, status=200)
--- application/preferences/manager.py
+++ application/preferences/manager.py
@@ -4,6 +4,13 @@ from application.preferences.validators import PreferencesRequestBody
 
 class PreferencesManager:
     async def set_preferences(self, preferences_body: PreferencesRequestBody) -> None:
+        """Set preferences
+
+        Save the preferences
+
+        Args:
+            preferences_body (PreferencesRequestBody): The preferences
+        """
         preferences = Preferences(
             to_language=preferences_body.to_language,
             from_language=preferences_body.from_language,
